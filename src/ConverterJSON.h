c50ffbcdd1291c2dd3af5555979e2900a934ef69
Maxim7972 <120516216+Maxim7972@users.noreply.github.com>
2024-12-17 15:07:13 +0300

    ConverterJSON.h

---
+++ src/ConverterJSON.h
@@ -0,0 +1,26 @@
+// include/ConverterJSON.h
+#pragma once
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+class ConverterJSON {
+public:
+    ConverterJSON(const std::string& configFilePath, const std::string& requestsFilePath, const std::string& answersFilePath);
+
+    std::vector<std::string> GetTextDocuments();
+    int GetResponsesLimit();
+    std::vector<std::string> GetRequests();
+    void PutAnswers(const std::vector<std::vector<std::pair<int, float>>>& answers);
+
+private:
+    nlohmann::json config_;
+    nlohmann::json requests_;
+    nlohmann::json answers_;
+
+    void LoadConfig();
+    void LoadRequests();
+    void LoadAnswers();
+};
