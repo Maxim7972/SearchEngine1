bb6c73d17f91119104202c7e34b322e73f0f0898
Maxim7972 <120516216+Maxim7972@users.noreply.github.com>
2025-01-14 10:35:57 +0300

    Update SearchServer.cpp

--- src/SearchServer.cpp
+++ src/SearchServer.cpp
@@ -1,13 +1,18 @@
 #include "SearchServer.h"
+#include "Logger.h"
 #include <algorithm>
 #include <sstream>
 
-SearchServer::SearchServer(const InvertedIndex& idx) : index(idx) {}
+SearchServer::SearchServer(const InvertedIndex& idx) : index(idx) {
+    Logger::GetInstance().Log("SearchServer initialized.");
+}
 
 std::vector<std::vector<RelativeIndex>> SearchServer::search(const std::vector<std::string>& queries_input) {
+    Logger::GetInstance().Log("Search started.");
     std::vector<std::vector<RelativeIndex>> result;
 
     for (const auto& query : queries_input) {
+        Logger::GetInstance().Log("Processing query: " + query);
         std::vector<RelativeIndex> query_results;
 
         std::istringstream stream(query);
@@ -27,5 +32,6 @@ std::vector<std::vector<RelativeIndex>> SearchServer::search(const std::vector<s
         result.push_back(query_results);
     }
 
+    Logger::GetInstance().Log("Search completed.");
     return result;
 }
