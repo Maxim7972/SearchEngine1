2d585302db2cee22cec325a7f3ffe7f528184d02
Maxim7972 <120516216+Maxim7972@users.noreply.github.com>
2025-01-14 10:35:12 +0300

    Update InvertedIndex.cpp

--- src/InvertedIndex.cpp
+++ src/InvertedIndex.cpp
@@ -1,7 +1,9 @@
 #include "InvertedIndex.h"
+#include "Logger.h"
 #include <sstream>
 
 void InvertedIndex::UpdateDocumentBase(const std::vector<std::string>& docs) {
+    Logger::GetInstance().Log("Updating document base.");
     for (size_t doc_id = 0; doc_id < docs.size(); ++doc_id) {
         std::istringstream stream(docs[doc_id]);
         std::string word;
@@ -9,6 +11,7 @@ void InvertedIndex::UpdateDocumentBase(const std::vector<std::string>& docs) {
             index[word].push_back(doc_id);
         }
     }
+    Logger::GetInstance().Log("Document base updated.");
 }
 
 const std::unordered_map<std::string, std::vector<size_t>>& InvertedIndex::GetIndex() const {
